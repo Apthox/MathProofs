ff1fd37065246fea6deb4e392915873160778c04
Paulo Jauregui <jauregui.paulo@gmail.com>
2018-06-19 15:42:05 -0700

    06/19/2018- added labels to graphs and included documentation

--- D3.py
+++ D3.py
@@ -25,16 +25,22 @@ m =.5
 
 
 #function f(h)
-def PLH_gain(H):
+def Prey_gain(H):
     return e**(-ke * H)
 
 #system where v is vector of x1, y, x2, z and t is time
 def system (v, t):
     X1, X2, Y, Z = v
+    #dx/dt yellow graph
+    dX1 = (-e1 * X1) - (c1 * Y * (X1 / (1 + X1))) + ((Prey_gain(H) * h1 * X1) * (1 - (X1 / n1))) + (f1 * ((X1 / (1 + X1))) * Z)
 
-    dX1 = (-e1 * X1) - (c1 * Y * (X1 / (1 + X1))) + ((PLH_gain(H) * h1*X1 ) * (1-(X1/n1))) + (f1 * ((X1 / (1 + X1))) * Z)
-    dX2 = (-e2 * X2) - (c2 * Y * (X2 / (1 + X2))) + (PLH_gain(H) * h2 * X2) + (f2 * ((X2 / (1 + X2))) * Z)
-    dY = (-a * Y) + ((b * X1) * (Y / (Y + 1)) * (1 - (Y / n))) + ((p *Y) * (1 / PLH_gain(H))) * (1 - (Y / n))
+    #dx2/dt red graph
+    dX2 = (-e2 * X2) - (c2 * Y * (X2 / (1 + X2))) + (Prey_gain(H) * h2 * X2) + (f2 * ((X2 / (1 + X2))) * Z)
+
+    #dy/dt cyan graph
+    dY = (-a * Y) + ((b * X1) * (Y / (Y + 1)) * (1 - (Y / n))) + ((p *Y) * (1 / Prey_gain(H))) * (1 - (Y / n))
+
+    #dz/dt green graph
     dZ = (f1 * X1) + (f2 * X2) - (m * Z)
 
     return [dX1, dX2, dY, dZ]
@@ -43,20 +49,36 @@ def system (v, t):
 fig, ax = plt.subplots()
 
 t = arange(0, 30, .1)
-#first parameter x1
-#second paramter y
-#third parameter 2
-#four parameter z
+    #[x1,x2, y, t]
 init = [20, 5, 2, 3]
 state = odeint(system, init, t)
+
+#plots dX1/dt graph
 plot(t, state[:, 0], 'y-')
+plt.ylabel('response')
+plt.xlabel('time')
+plt.title("dX1/dt")
 figure()
 
+#plots dX2/dt graph
 plot(t, state[:, 1], 'r-')
+plt.ylabel('response')
+plt.xlabel('time')
+plt.title("dX2/dt")
 figure()
+
+#plots dY/dt graph
 plot(t, state[:, 2], 'c-')
+plt.ylabel('response')
+plt.xlabel('time')
+plt.title("dY/dt")
 figure()
+
+#plots dZ/dt graph
 plot(t, state[:, 3], 'g-')
+plt.ylabel('response')
+plt.xlabel('time')
+plt.title("dZ/dt")
 
 
 #l11, = plt.plot(state[:, 0], state[:, 1], 'b-')
