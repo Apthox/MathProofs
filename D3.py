8de263f19949d7ee71d33d806fee25956c6894a4
Paulo Jauregui <jauregui.paulo@gmail.com>
2018-06-19 13:40:40 -0700

    06/19/2018- End of day with eq

--- D3.py
+++ D3.py
@@ -4,22 +4,22 @@ from numpy import arange
 from pylab import *
 from matplotlib.widgets import Slider, Button, RadioButtons
 
-a = .105
-b = .15
-c = .3
-p = .5
-n = 5
-ke = .1635
+a = .105 #predator adult mortality rate
+b = .15 #predator adult consumption rate of weevil
+c = .3  #predator adult consumption aphids
+p = .5  #predator adult pop gain from numphs
+n = 10
+n1 = 100
+ke = .1635  #constant
 H = .5
-c1 =.01
-c2 =.5
-h1 =.5
-h2 =.5
-e1 =.5
-e2 =.5
+c1 =.3
+c2 =.05
+h1 =.2
+h2 =.2
+e1 =.5 #mortality of aphids
+e2 =.5 #mortality of weevil
 H =.5
-ke =.5
-f1 =.5
+f1 =.5  #adult pest consumption rate of alfalfa
 f2 =.5
 m =.5
 
@@ -32,8 +32,8 @@ def PLH_gain(H):
 def system (v, t):
     X1, X2, Y, Z = v
 
-    dX1 = (-e1 * X1) - (c1 * Y * (X1)) + (PLH_gain(H) * h1*X1) + (f1 * ((X1 / (1 + X1))) * Z)
-    dX2 = (-e2 * X2) - (c2 * Y * (X2)) + (PLH_gain(H) * h2 * X2) + (f2 * ((X2 / (1 + X2))) * Z)
+    dX1 = (-e1 * X1) - (c1 * Y * (X1 / (1 + X1))) + ((PLH_gain(H) * h1*X1 ) * (1-(X1/n1))) + (f1 * ((X1 / (1 + X1))) * Z)
+    dX2 = (-e2 * X2) - (c2 * Y * (X2 / (1 + X2))) + (PLH_gain(H) * h2 * X2) + (f2 * ((X2 / (1 + X2))) * Z)
     dY = (-a * Y) + ((b * X1) * (Y / (Y + 1)) * (1 - (Y / n))) + ((p *Y) * (1 / PLH_gain(H))) * (1 - (Y / n))
     dZ = (f1 * X1) + (f2 * X2) - (m * Z)
 
@@ -47,7 +47,7 @@ t = arange(0, 30, .1)
 #second paramter y
 #third parameter 2
 #four parameter z
-init = [1, 1, 2, 3]
+init = [20, 5, 2, 3]
 state = odeint(system, init, t)
 plot(t, state[:, 0], 'y-')
 figure()
