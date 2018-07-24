a04b690c1ab0a4a556329b4c5dc749b9dd333dcc
apthos <America99>
2018-07-24 12:12:56 -0700

    July 24th - Jacobi Matrix

--- JacobianMatrix.py
+++ JacobianMatrix.py
@@ -7,57 +7,6 @@ d, c1, ke, H, l, j = var('d c1 ke H l j')
 e2, c2, c, h2, f2 = var('e2 c2 c h2 f2')
 a, b, n, p, m, e = var('a b n p m e')
 
-a = .105  # predator adult mortality rate
-range_a = [0.01, 1]
-
-b = 0.1  # Benefit to the predator of predator adult consumption rate of weevil
-range_b = [.01, 1]
-
-c = 0.3  # Negative impact of early harvesting on the weevil population (not being used)
-range_c = [.01, 1]
-
-p = .5  # predator adult population gain from nymphs
-range_p = [.01, 1]
-
-n = 10
-range_n = [1, 10]
-
-k = 100
-range_n1 = [1, 250]
-
-ke = .1635  # constant
-
-H = .8
-range_H = [.01, 1]
-
-c1 = .15  # Negative impact on the aphids of predator consumption of aphids; previous values used: .1,2
-range_c1 = [.01, 1]
-
-# !
-c2 = .05  # Negative impact on the weevil of predator consumption of weevil; previous values used: .03
-range_c2 = [.01, 1]
-
-j = .5    # adult population gain (aphids)  # previous values used: .2
-range_h1 = [.01, 1]
-
-h2 = .3     # adult population gain (weevil) previous values used: .2
-range_h2 = [.01, 1]
-
-d = .15  # mortality of aphids; previous values used: .15, .3
-range_e1 = [.01, 1]
-
-# e2 = .15  # mortality of weevil; previous values used: .15, .3
-# range_e2 = [.01, 1]
-
-l = .2  # aphids pest consumption rate of alfalfa; previous values used: .15
-range_f1 = [.01, 1]
-
-f2 = .2  # weevil pest consumption rate of alfalfa; previous values used: .15,
-range_f2 = [.01, 1]
-
-m = .5
-range_m = [.01, 1]
-
 E1 = (-d * X1) - (c1 * Y * (X1 / (1 + X1))) + (e ** (-ke * H) * j * (X1 / (1 + X1))) + (l * (X1 / (1 + X1)) * Z)
 E2 = (-e2 * X2) - (c2 * Y * (X2 / (1 + X2))) + (c * e ** (-ke * H) * h2 * (X2 / (1 + X2))) + (f2 * (X2 / (1 + X2)) * Z)
 E3 = (-a * Y) + ((b * X1) * (Y / (Y + 1)) * (1 - (Y / n))) + ((p * (1 / e ** (-ke * H))) * (Y / (Y + 1)))
@@ -83,21 +32,43 @@ A42 = diff(E4, X2)
 A43 = diff(E4, Y)
 A44 = diff(E4, Z)
 
-# a = .105
-# b = 0.1
-# c = 0.3
-# p = .5
-# n = 10
-# k = 100
-# ke = .1635
-# H = .8
-# c1 = .15
-# c2 = .05
-# j = .5
-# h2 = .3
-# d = .15
-# e2 = .15
-# l = .2
+print("A11: " + str(A11))
+print("A12: " + str(A12))
+print("A13: " + str(A13))
+print("A14: " + str(A14))
+
+print("A21: " + str(A21))
+print("A22: " + str(A22))
+print("A23: " + str(A23))
+print("A24: " + str(A24))
+
+print("A31: " + str(A31))
+print("A32: " + str(A32))
+print("A33: " + str(A33))
+print("A34: " + str(A34))
+
+print("A41: " + str(A41))
+print("A42: " + str(A42))
+print("A43: " + str(A43))
+print("A44: " + str(A44))
+
+a = .105
+b = 0.1
+c = 0.3
+p = .5
+n = 10
+k = 100
+ke = .1635
+H = .8
+c1 = .15
+c2 = .05
+j = .5
+h2 = .3
+d = .15
+e2 = .15
+l = .2
+f2 = .2
+m = .5
 
 X1 = 0
 X2 = 0
